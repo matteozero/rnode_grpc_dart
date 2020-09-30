556dc51315e3edce0ca43f47d4bd64ab40bbcf4b
matteo <immatteoz@gmail.com>
2020-09-30 16:33:59 +0800

    .

--- lib/rnode_grpc_dart.dart
+++ lib/rnode_grpc_dart.dart
@@ -1,7 +1,3 @@
 library rnode_grpc_dart;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:rnode_grpc_dart/src/rnode_grpc/rnode_grpc.dart';
